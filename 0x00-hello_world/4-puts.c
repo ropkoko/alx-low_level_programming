3a3375aa5ef3eaaf540efdecbb8b08d19a502192
ropkoko <ropbrenda4@gmail.com>
2023-07-24 03:30:53 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -Programming is like building a multilingual puzzle
+ * Return: 0 (succes)
+ */
+
+int main(void)
+{
+	puts("\"exactly the line");
+	return (0);
+}
